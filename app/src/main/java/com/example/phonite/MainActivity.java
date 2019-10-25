7f349a25f4f72a4ca90cbefa461d260156dfb91e
chairithinug <chairithinug@wisc.edu>
2019-10-25 15:35:11 -0500

    Camera API

--- app/src/main/java/com/example/phonite/MainActivity.java
+++ app/src/main/java/com/example/phonite/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.phonite;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.hardware.camera2;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final String TAG = "MAIN";
+    public TextView tvTime;
+    public Button btnSt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvTime = (TextView) findViewById(R.id.tvTime);
+        btnSt = (Button) findViewById(R.id.btnSt);
+
+
     }
 }
