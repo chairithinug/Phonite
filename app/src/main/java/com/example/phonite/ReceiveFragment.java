2473d7fbf55d3d7ea063813a02f9fc7ee2a9f3eb
Alex Yavnilovitch <yavnilovitch@wisc.edu>
2019-11-04 17:21:00 -0600

    Adding front camera brightness detection

--- app/src/main/java/com/example/phonite/ReceiveFragment.java
+++ app/src/main/java/com/example/phonite/ReceiveFragment.java
@@ -1,23 +1,39 @@
 package com.example.phonite;
 
 
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.os.SystemClock;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ReceiveFragment extends Fragment {
-
+    TextView textLight;
+    TextView hit;
+    SensorManager sensorManager;
+    Sensor sensor;
+    String sx;
+    float prevBrightness;
 
     public ReceiveFragment() {
-        // Required empty public constructor
+        // empty public constructor
     }
 
 
@@ -25,7 +41,59 @@ public class ReceiveFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_receive, container, false);
+        View view = inflater.inflate(R.layout.fragment_receive, container, false);
+        sensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        textLight = (TextView) view.findViewById(R.id.textLight);
+        hit = (TextView) view.findViewById(R.id.hit);
+        hit.setText("");
+        return view;
     }
+
+    public void onResume() {
+        super.onResume();
+        sensorManager.registerListener(lightListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(lightListener);
+    }
+
+    public SensorEventListener lightListener = new SensorEventListener() {
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            if (x > 2*prevBrightness && x > 150) {
+                Log.d("onSensorChanged FLS", String.valueOf(x));
+                hit.setTextColor(Color.parseColor("#ff0000"));
+                hit.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.d(TAG,"printed HIT?");
+                        hit.setText("HIT");
+                    }
+                }, 600);
+            }
+            hit.post(new Runnable() {
+                @Override
+                public void run() {
+                    hit.setText("");
+                }
+            });
+
+            sx = String.valueOf(x);
+
+            textLight.post(new Runnable() {
+                @Override
+                public void run() {
+                    textLight.setText((CharSequence) sx);
+                }
+            });
+            prevBrightness = x;
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int acc) {
+        }
+    };
 
 }
