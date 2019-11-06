5926ef6ee8de93e47fb72b30c0b9cb7689e9ef20
chairithinug <chairithinug@wisc.edu>
2019-11-06 16:46:56 -0600

    labeling works!

--- app/src/main/java/com/example/phonite/CameraStreamer.java
+++ app/src/main/java/com/example/phonite/CameraStreamer.java
@@ -1,4 +1,4 @@
-package com.example.phonite;
+    package com.example.phonite;
 
 import android.Manifest;
 import android.content.Context;
--- app/src/main/java/com/example/phonite/ReceiveFragment.java
+++ app/src/main/java/com/example/phonite/ReceiveFragment.java
@@ -1,13 +1,29 @@
 package com.example.phonite;
 
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.objects.FirebaseVisionObject;
+import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetector;
+import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetectorOptions;
+
+import java.util.List;
 
 
 /**
@@ -15,6 +31,11 @@ import android.view.ViewGroup;
  */
 public class ReceiveFragment extends Fragment {
 
+    public final String TAG = "Analyze";
+
+    public TextView modeTextR;
+    public Button btnAnalyze;
+    private Context context;
 
     public ReceiveFragment() {
         // Required empty public constructor
@@ -25,7 +46,23 @@ public class ReceiveFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_receive, container, false);
+        View view = inflater.inflate(R.layout.fragment_receive, container, false);
+
+
+        modeTextR = view.findViewById(R.id.modeTextR);
+        btnAnalyze = view.findViewById(R.id.btnAnalyze);
+        context = getActivity().getApplicationContext();
+
+        btnAnalyze.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Camera click");
+                ImageAnalyzer imageAnalyzer = new ImageAnalyzer(context);
+                imageAnalyzer.analyze(null,0);
+            }
+        });
+
+        return view;
     }
 
 }
